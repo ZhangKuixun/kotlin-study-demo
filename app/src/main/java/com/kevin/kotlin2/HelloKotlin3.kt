591a7d256043f0be63c07e860fc3a9dd6a77648b
ZhangKuixun <1129826190@qq.com>
2021-06-28 15:54:24 +0800

    no message

--- app/src/main/java/com/kevin/kotlin2/HelloKotlin3.kt
+++ app/src/main/java/com/kevin/kotlin2/HelloKotlin3.kt
@@ -400,19 +400,13 @@ fun <T> getValue(item: T): T {
     return item
 }
 
-fun main() {
-    val item = getValue<Int>(3)
-    println(item)// 3
-
-    val item2 = getValue("hello")
-    println(item2)// hello
-
-    ///////////上界
-    val upperBoundsClass = UpperBoundsClass<AbstractList<String>>()
-
-    //////////多个上界
-    val upperBoundsClass2 = UpperBoundsClass2<String>()
-}
+//fun main() {
+//    val item = getValue<Int>(3)
+//    println(item)// 3
+//
+//    val item2 = getValue("hello")
+//    println(item2)// hello
+//}
 
 /**
  * <T>表示这个方法是泛型方法，它拥有一个方法泛型类型T，所以将参数声明为T类型，并且返回T类型，最后return item就符合T类型
@@ -421,7 +415,10 @@ fun main() {
 
 class UpperBoundsClass<T : List<T>> {
 }
-
+fun main() {
+    //上界
+    val upperBoundsClass = UpperBoundsClass<AbstractList<String>>()
+}
 /**
  * 定义好一个泛型，可以定义一个泛型的上界。
  * UpperBoundsClass<T : List<T>>：T的上界就是List。
@@ -431,6 +428,10 @@ class UpperBoundsClass<T : List<T>> {
 
 class UpperBoundsClass2<T> where T : Comparable<T>, T : Any {
 }
+//fun main() {
+//    //多个上界
+//    val upperBoundsClass2 = UpperBoundsClass2<String>()
+//}
 /**
  * 两个及两个以上的上界
  * where T : Comparable<T>, T : Any，既要满足T : Comparable<T>，又要满足T : Any，后面的 T : Any 可以不用写

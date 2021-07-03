094203bb5513606ec39609f0515a6f6d431c008b
DESKTOP-ODVQB7N\kevin <zhangkuixun@gmail.com>
2021-07-03 18:33:11 +0800

    no message

--- app/src/main/java/com/kevin/kotlin/HelloKotlin6.kt
+++ app/src/main/java/com/kevin/kotlin/HelloKotlin6.kt
@@ -5,27 +5,7 @@ package com.kevin.kotlin
  * create time：2021/6/20
  * describe：扩展
  */
-
-fun main() {
-    val extensionTest = ExtensionTest()
-    println(extensionTest.add(1, 2))// 3
-    println(extensionTest.subtract(1, 2))// -1
-    println(extensionTest.multiply(1, 2))// 2
-
-    println("-----------")
-
-    myPrint(AA())
-
-    println("-----------")
-
-    CC().foo()
-
-    println("-----------")
-
-    EE().world(DD())
-    EE().test()
-}
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // 扩展
 // 什么是扩展：已经定义好了一个类，想要给这个类扩充功能，类似于Java的继承、装饰模式(io流操作，想读什么流就读什么流)
 class ExtensionTest {
@@ -36,6 +16,15 @@ class ExtensionTest {
 // 扩展方法
 fun ExtensionTest.multiply(a: Int, b: Int) = a * b
 
+//fun main() {
+//    val extensionTest = ExtensionTest()
+//    println(extensionTest.add(1, 2))// 3
+//    println(extensionTest.subtract(1, 2))// -1
+//    println(extensionTest.multiply(1, 2))// 2
+//}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // 扩展
 // 1.扩展并不是真正修改原来的类，不会在原来的类中插入属性或方法
 // 2.扩展函数的解析是静态的，而不是动态的，即扩展不支持多态（通过引用父类去指向子类对象），调用只取决于对象的声明类型
@@ -49,10 +38,14 @@ fun myPrint(aa: AA) {
     println(aa.a())
 }
 
+//fun main() {
+//    myPrint(AA())
+//}
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // 扩展重名的方法
-//   优先使用原方法，原方法不满足，在使用扩展方法
-//   调用：CC().foo()// 输出：member
+// 优先使用原方法，原方法不满足，在使用扩展方法
+// 调用：CC().foo()// 输出：member
 class CC {
     fun foo() {
         println("member")
@@ -63,30 +56,33 @@ fun CC.foo() {
     println("member2")
 }
 
+//fun main() {
+//    CC().foo()
+//}
 
-// 对可控类型扩展，可控类型：一个类型后面加一个问号
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// 对可控类型扩展，可空类型：一个类型后面加一个问号
 fun Any?.toString(): String {
     if (null == this)
         return "null"
     return toString()
 }
 
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // 扩展的作用域
-
-/*
-* 扩展的定义
-*   1.扩展函数定义在某个类，这个类实列叫做分发接收者（dispatch receiver）
-*   2.扩展函数所扩展的目标类，这个类实例叫做扩展接收者（extension receiver）
-*
-* 扩展的作用域
-*   1.在另一个类里面（分发接收者），定义了一个扩展函数，在这个扩展函数中可以访问扩展接收者的函数，也可以访问分发接收者的成员
-*   2.在分发接收者里的函数里，可以调用在本类中定义的扩展函数
-*   3.当以上分发接收者或者扩展接收者里的成员名字出现冲突时，扩展接收者的优先级最高
-*       3.1.如果想要调用自己的函数名，写法：this@EE.tos()
-* */
+/**
+ * 扩展的定义
+ *   1.扩展函数定义在某个类，这个类实列叫做分发接收者（dispatch receiver）
+ *   2.扩展函数所扩展的目标类，这个类实例叫做扩展接收者（extension receiver）
+ *
+ * 扩展的作用域
+ *   1.在另一个类里面（分发接收者），定义了一个扩展函数，在这个扩展函数中可以访问扩展接收者的函数，也可以访问分发接收者的成员
+ *   2.在分发接收者的函数中，可以调用在本类中定义的扩展函数
+ *   3.当分发接收者或者扩展接收者里的成员名字出现冲突时，扩展接收者的优先级最高
+ *   3.1.如果想要调用自己的函数名，写法：this@EE.tos()
+ */
 class DD {
+    //扩展接收者
     fun method() {
         println("DD method")
     }
@@ -97,8 +93,8 @@ class DD {
 }
 
 class EE {
+    //分发接收者
     fun method2() {
-
     }
 
     fun DD.hello() {
@@ -118,8 +114,7 @@ class EE {
         DD().output()
     }
 
-    // 调用
-    //   EE().test()
+    // EE().test()
     // 输出：
     //   DD fun
     //   EE fun
@@ -129,10 +124,15 @@ class EE {
     }
 }
 
-/*
-* 扩展的作用
-*   解决Java中充斥的各种辅助类问题，比如列表的交换方法，必须要把列表传进辅助类里，在辅助类里排序
-* 语法改进：
-*   Java原语法：Collections.swap(list, 4, 10)
-*   kotlin改进语法：list.swap(4, 10)
-* */
+fun main() {
+    EE().world(DD())
+    EE().test()
+}
+
+/**
+ * 扩展的作用
+ *   解决Java中充斥的各种辅助类问题，比如列表的交换方法，必须要把列表传进辅助类里，在辅助类里排序
+ * 语法改进：
+ *   Java原语法：Collections.swap(list, 4, 10)
+ *   kotlin改进语法：list.swap(4, 10)
+ */
--- app/src/main/java/com/kevin/kotlin3/HelloKotlin3.kt
+++ app/src/main/java/com/kevin/kotlin3/HelloKotlin3.kt
@@ -16,11 +16,39 @@ package com.kevin.kotlin3
  * 在kotlin中，如果一个函数的最后一个参数是函数，可以将lambda作为参数，传递到函数中，并且lambda可以写在圆括号外面。
  */
 
-val multiply: (Int, Int) -> Int = { a, b -> a * b }//冒号后面是类型，等号后面是值
+val multiply: (Int, Int) -> Int = { a, b -> a * b }//冒号后面是类型，等号后面是值（常用）
 val add: (Int, Int) -> Int = { a, b -> a + b }
-val subtract = { a: Int, b: Int -> a - b }//推断类型
+val subtract = { a: Int, b: Int -> a - b }//推断类型（常用）
 val myAction = { println("hello world") }//返回Unit
 
 val mayReturnNull: (Int, Int) -> Int? = { _, _ -> null }//返回值为空
 
 val functionMaybeNull: ((Int, Int) -> Int)? = null//整个函数类型都可能为null
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// 高阶函数
+/**
+ * 高阶函数（high-order function）
+ * 定义一个函数，其中一个参数也是一个函数。
+ */
+fun myCalculate(a: Int, b: Int, calculate: (Int, Int) -> Int): Int {
+    return calculate(a, b)
+}
+
+/**
+ * 分析：myCalculate有三个参数，参数1和参数2，参数3是一个函数，这个函数可以传入两个Int型的参数，返回一个Int型的值，
+ * myCalculate整个函数返回一个Int值。
+ */
+
+fun main() {
+    println(myCalculate(2, 3) { x, y -> x + y })//5
+    println(myCalculate(2, 3) { x, y -> x - y })//-1
+    println(myCalculate(2, 3) { x, y -> x * y })//6
+}
+
+/**
+ *
+ */
+
+
+

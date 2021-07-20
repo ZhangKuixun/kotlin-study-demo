2f87e5c653de28b08f55451e6751f9da3e2826a2
ZhangKuixun <1129826190@qq.com>
2021-07-20 18:08:24 +0800

    no message

---
+++ app/src/main/java/com/kevin/kotlin10/HelloKotlin10.kt
@@ -0,0 +1,22 @@
+package com.kevin.kotlin10
+
+/**
+ * other：Kevin
+ * create time：2021/7/20
+ * describe：
+ * 引用特定对象的一个实例方法，也可以引用特定对象的属性。
+ */
+
+fun main() {
+    val str = "abc"
+    val getReference = str::get
+    println(getReference(1))//b
+
+    val length = "test"::length
+    println(length.get())//4
+
+    val length1 = String::length.get("test")
+    println(length1)
+}
+
+

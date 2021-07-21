4391815dc73f7faeb760b73fd31794efe9c82e07
ZhangKuixun <1129826190@qq.com>
2021-07-21 11:53:49 +0800

    no message

---
+++ app/src/main/java/com/kevin/kotlin10/HelloKotlin18.kt
@@ -0,0 +1,23 @@
+package com.kevin.kotlin10
+
+import kotlin.reflect.full.memberProperties
+
+/**
+ * other：Kevin
+ * create time：2021/7/21
+ * describe：
+ * 用反射读取属性
+ */
+class MyTestClass6 {
+    var name: String = "hello"
+}
+
+fun main() {
+    val kClass = MyTestClass6::class
+    val myTestClass6 = MyTestClass6()
+    val variableToInvoke = kClass.memberProperties.find { it.name == "name" }
+    println(variableToInvoke?.get(myTestClass6))//hello
+    println(variableToInvoke?.call(myTestClass6))//hello
+}
+/**
+ */

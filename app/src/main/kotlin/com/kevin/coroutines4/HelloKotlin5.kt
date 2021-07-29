e19716355fb199204721398d49016d66913d45c5
ZhangKuixun <1129826190@qq.com>
2021-07-29 21:46:14 +0800

    no message

--- app/src/main/kotlin/com/kevin/coroutines4/HelloKotlin5.kt
+++ app/src/main/kotlin/com/kevin/coroutines4/HelloKotlin5.kt
@@ -1,6 +1,7 @@
 package com.kevin.coroutines4
 
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -10,14 +11,16 @@ import kotlinx.coroutines.runBlocking
  * describe：
  * Job的使用
  * 如果在协程的代码块中，如何获取Job本身？
- * 协程的Job属于协程上下文（coroutineContext）的一部分：
- * val job: Job? = this.coroutineContext[Job]
+ * 协程的Job属于协程上下文（coroutineContext）的一部分：通过this.coroutineContext[Job]表达式获取Job对象。
  *
  */
 fun main() = runBlocking<Unit> {
-    val job: Job? = coroutineContext[Job]
+    val job: Job? = this.coroutineContext[Job]
     println(job)
     //打印：BlockingCoroutine{Active}@6e5e91e4
+
+    println(coroutineContext.isActive)//true
+    println(coroutineContext[Job]?.isActive)//true
 }
 /**
  * 分析：
@@ -28,7 +31,21 @@ fun main() = runBlocking<Unit> {
  * coroutineContext是CoroutineScope的变量，runBlocking是一个可阻塞的协程，runBlocking最终
  * 调用BlockingCoroutine，它继承了CoroutineScope，所以可以去使用。
  *
+ * CoroutineScope的coroutineContext
+ * 文档：
+ * 协程作用域的上下文，上下文通过一个封装，并且用于协程构建器的实现，构建器实际是作用域的一个扩展。
+ * 通常的代码中不建议被使用，除了访问Job实例。根据约定，应该包含一个Job的实例去强制的结构化的并发。
+ *
  * CoroutineContext：
  * 文档：
+ * 协程的持久上下文。它是[Element]实例的索引集合，索引集合是介于set和map之间，索引集合的每一个
+ * 元素都有唯一的key。
+ *
+ *
+ * 为什么能在这里用Job？
+ * public interface Job : CoroutineContext.Element {
+ *     public companion object Key : CoroutineContext.Key<Job> {}
+ * }
+ * 因为在Job里面定义了一个伴生对象Key，CoroutineContext.Key<Job>key里面的泛型就是Job。
  *
  */
---
+++ app/src/main/kotlin/com/kevin/coroutines4/HelloKotlin6.kt
@@ -0,0 +1,50 @@
+package com.kevin.coroutines4
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * other：Kevin
+ * create time：2021/7/29
+ * describe：
+ * 父子协程的关系
+ *
+ * 在一个协程内部用标砖的方式（launch、async）启动一个协程，内部的协程会通过
+ * CoroutineScope.coroutineContext继承外部协程的上下文，同时，内部协程的Job会成为外部协程
+ * Job的一个孩子，内部协程会受到外部协程的影响，父协程被取消执行时，父协程所有的子协程会通过递
+ * 归的方式一并取消执行。可以无限嵌套。
+ *
+ * 特例情况：如果在一个协程内部用GlobalScope启动一个新协程，内部协程不是外部协程的一个孩子，内
+ * 部协程的Job没有父Job，它不会绑定到它所启动的父协程范围上，内部的协程就是一个独立的协程。
+ *
+ * GlobalScope：
+ * 文档：
+ * 1.GlobalScope实现CoroutineScope接口，是全局的CoroutineScope，不会绑定到任何的Job上。
+ * 2.GlobalScope用于启动顶层的协程，顶层的协程是在整个应用的生命周期中操作，而且不会被取消。
+ * 3.另一种用法，运行在Dispatchers.Unconfined，没有任何关联的Job。
+ *
+ * 开发者写代码时，应该使用应用定义CoroutineScope，在GlobalScope实例上非常不建议使用async、launch。
+ */
+fun main() = runBlocking<Unit> {
+    val request = launch {
+        GlobalScope.launch {
+            println("job1:hello")
+            delay(1000)
+            println("job2:world")
+        }
+
+        launch {
+            delay(100)
+            println("job2:hello")
+            delay(1000)
+            println("job2:world")
+        }
+    }
+    delay(500)
+    request.cancel()
+
+    delay(1000)
+    println("welcome")
+}

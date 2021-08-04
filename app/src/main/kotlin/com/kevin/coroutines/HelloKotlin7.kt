0552f6944a6cb67031e40d6110386916ac52c081
ZhangKuixun <1129826190@qq.com>
2021-08-04 22:17:05 +0800

    no message

--- app/src/main/kotlin/com/kevin/coroutines/HelloKotlin7.kt
+++ app/src/main/kotlin/com/kevin/coroutines/HelloKotlin7.kt
@@ -8,9 +8,6 @@ import kotlinx.coroutines.*
  * create time：2021/7/25
  * describe：
  *
- * 除去不同的协程构建器所提供的协程作用域（coroutine scope）外，还可以通过coroutineScope builder
- * 来声明自己的作用域，该构造器会创建一个协程作用域，并且会等待所有启动的子协程全部完成后自身才会完成。
- *
  * runBlocking与coroutineScope的区别，coroutineScope在等待所有子协程完成任务时不会阻塞当前线程。
  *
  * 1.runBlocking是普通函数，不是挂起函数；调用它的线程会一直位于runBlocking函数中，直到协程执行完毕。
@@ -37,18 +34,24 @@ fun main() = runBlocking {
     //打印：person；1秒后打印"my job1"；5秒后打印"hello world"；20秒后打印"my job2"；打印"welcome"；
 }
 /**
- * 程序进入runBlocking之后，第一行代码是启动协程的函数launch{}，launch{}不会阻塞当前线程，当前线程往下走，
- * 打印"person"，然后执行coroutineScope{}，coroutineScope{}是一个挂起函数，它会等待自己的所有子协程全部
- * 执行完之后，它自己才标记为执行完成。对于当前线程来说，遇到了coroutineScope{}，当coroutineScope{}没有
- * 执行完毕，当前线程不会执行coroutineScope{}的下一行代码，不会打印"welcome"。
+ *
+ * 分析：
+ * 1 除去不同的协程构建器创建的协程作用域外，还可以用coroutineScope构造函数来声明自己的作用域，该构造
+ * 器会创建一个协程作用域，并且会等待所有启动的子协程全部完成后自身才会完成。
+ * 2 程序进入runBlocking之后，第一行代码是启动协程launch{}，launch{}不阻塞线程，当前线程往下走，打
+ * 印"person"，然后执行协程构造函数coroutineScope{}，coroutineScope{}是一个挂起函数，它会等待自己
+ * 的所有子协程全部执行完之后，它自己才执行完成。对于当程来说，遇到了coroutineScope{}，当
+ * coroutineScope{}没有执行完毕，当前线程不会执行coroutineScope{}的下一行代码，不会打印"welcome"。
+ *
  * 这和之前的总结有冲突：coroutineScope在等待所有子协程完成任务时不会阻塞当前线程。
  *
  * 为什么会这样呢？
- * 首先coroutineScope是一个挂起函数，等到自己里面所有的子协程完成之后，主代码才会继续往下走，但是对于当
- * 前线程来说，一旦遇到了coroutineScope，coroutineScope会把里面每一行代码都走完后，当前线程就从
- * coroutineScope中返回到外层，外层就是runBlocking的执行体，注意：不是返回到coroutineScope{}下一行
- * 的代码而是返回到coroutineScope{}上一行的代码。所以上面的总结是正确的，coroutineScope不会阻塞当前
- * 线程。如果当前线程被阻塞，不会打印"my job1"。
+ * 结论：首先coroutineScope{}是一个挂起函数，等到自己里面所有的子协程完成之后，主代码才会继续往下走。
+ * 结合程序分析：对于线程来说，一旦遇到了coroutineScope{}，会把coroutineScope{}里面每一行代码都执行
+ * 完后，如果coroutineScope{}里面有挂起函数，也要等到挂起函数执行完，线程就从coroutineScope{}中返回
+ * 到外层，外层是runBlocking的执行体。
+ * 注意：不是返回到coroutineScope{}下一行的代码而是返回到coroutineScope{}上一行的代码。所以上面的总
+ * 结是正确的，coroutineScope不会阻塞线程。如果线程被阻塞，不会打印"my job1"。
  *
  * 当前线程在做什么呢？
  * runBlocking有一个事件循环的概念，因为runBlocking里面，在未来的一段时间可能还有其他会被执行的代码。
